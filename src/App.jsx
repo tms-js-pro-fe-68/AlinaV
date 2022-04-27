9bd36c2fca607fdc86019784f2642cce6d5beb8a
vinnipuhl <whowhowholin@gmail.com>
2022-04-27 22:08:31 +0300

    LoginPage_formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 
-// const LoginPage = lazy(() => import('./pages/LoginPage')) //для асинх подгрузки*
-
 export default function App() {
-  return <Router>
-    <Routes>
-      <Route path='/login' element={<LoginPage/>}/>
-    </Routes>
-  </Router>
+  return(
+    <Router>
+      <Routes>
+        <Route path='/login' element={<LoginPage/>}>
+        </Route>
+        <Route path='/' element='homepage' />
+      </Routes>
+    </Router>
+  )
 }
