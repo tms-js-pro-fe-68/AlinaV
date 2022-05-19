cb79c462970e74a046f7b7cb71ef48c1f9505eb8
vinnipuhl <92868313+vinnipuhl@users.noreply.github.com>
2022-05-19 21:55:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import LoginPage from './pages/LoginPage.jsx'
-import HomePage from './pages/homePage/HomePage'
+import HomePage from './pages/homePage/HomePage.jsx'
 
 export default function App() {
   return(
