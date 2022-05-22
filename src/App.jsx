2d4d93e5ce32c38bbba2a3cfb3cb2c8d29af3710
vinnipuhl <whowhowholin@gmail.com>
2022-05-22 18:05:34 +0300

    Query

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Route, Router, Routes } from 'react-router-dom';
 
 export default function App() {
 
-  const queryClient = QueryClient({
+  const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: Infinity,
